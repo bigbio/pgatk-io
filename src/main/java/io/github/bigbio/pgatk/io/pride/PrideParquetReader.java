596f1471bde8c4e6c4bd52597895b9a07489e3a3
ypriverol <ypriverol@gmail.com>
2021-04-28 09:52:19 +0100

    improving ArchiveSpectrum with samples

--- src/main/java/io/github/bigbio/pgatk/io/pride/PrideParquetReader.java
+++ src/main/java/io/github/bigbio/pgatk/io/pride/PrideParquetReader.java
@@ -6,6 +6,8 @@ import org.apache.avro.reflect.ReflectData;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.parquet.avro.AvroParquetReader;
+import org.apache.parquet.filter2.compat.FilterCompat;
+import org.apache.parquet.filter2.predicate.FilterPredicate;
 import org.apache.parquet.hadoop.ParquetReader;
 import java.io.File;
 import java.io.IOException;
@@ -40,6 +42,32 @@ public class PrideParquetReader {
   }
 
   /**
+   * Constructor of {@link PrideParquetReader} using a {@link FilterPredicate} which allows to filter data by some specific
+   * fileds. For example, the user can retrieve only {@link AnnotatedSpectrum} that agreed with the Filter conditions.
+   * @param source File to be read
+   * @param filter A {@link FilterPredicate}
+   * @throws PgatkIOException
+   */
+  public PrideParquetReader(File source, FilterPredicate filter) throws PgatkIOException {
+
+    this.path = new Path(source.getPath());
+    this.conf = new Configuration(false);
+    this.schema = ReflectData.get().getSchema(AnnotatedSpectrum.class);
+
+    try {
+      this.reader = AvroParquetReader.<AnnotatedSpectrum>builder(this.path)
+        .withDataModel(new ReflectData(AnnotatedSpectrum.class.getClassLoader()))
+        .disableCompatibility()
+        .withConf(conf)
+        .withFilter(FilterCompat.get(filter))
+        .build();
+    } catch (IOException e) {
+      throw new PgatkIOException("Error creating the output file in path: " +
+        source.getAbsolutePath(), e);
+    }
+  }
+
+  /**
    * Method to read an {@link AnnotatedSpectrum} from parquet file
    * @return An {@link AnnotatedSpectrum}
    * @throws PgatkIOException
@@ -53,6 +81,8 @@ public class PrideParquetReader {
     }
     return spec;
   }
+
+//
 
   public void close() throws PgatkIOException {
     try {
